e8f70d689d8dae4309154cd01afb36fb0f3e127e
Serdar Kadioglu <serdar.kadioglu@fmr.com>
2024-05-14 22:27:42 -0400

    update

--- pypi.sh
+++ pypi.sh
@@ -1,4 +1,6 @@
-pip install twine # if twine is not installed
-pip install setuptools wheel # if wheel is not installed
+#!/bin/bash
+
+# pip install twine # if twine is not installed
+# pip install setuptools wheel # if wheel is not installed
 python setup.py sdist bdist_wheel
 twine upload dist/* --verbose
